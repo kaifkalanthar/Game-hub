8cf66f4b443f478feefea1b1c498683e85cd8d1d
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-19 18:04:19 +0530

    icons added

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,33 @@
+import { Icon } from "@chakra-ui/react";
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa'
+import { MdPhoneIphone } from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import { BsGlobe } from 'react-icons/bs'
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+
+interface Props {
+    platform: Platform[];
+}
+
+const PlatformIconList = ({ platform }: Props) => {
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
+    return (
+        <>
+            {platform.map(platform => <Icon color='grey.500' margin={1} as={iconMap[platform.slug]} key={platform.id} />)}
+        </>
+    )
+}
+
+export default PlatformIconList
