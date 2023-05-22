ff971764a1027d2c675492d0044ded751cf62bd0
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-22 18:54:54 +0530

    filter done

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,17 +1,34 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 
-const SortSelector = () => {
+interface Props {
+    sortOrder: string;
+    onSelectedSortOrder: (sortOrder: string) => void;
+}
+const SortSelector = ({ sortOrder, onSelectedSortOrder }: Props) => {
+
+    const sortOrders = [
+        { value: '', label: 'Relevance' },
+        { value: '-added', label: 'Data added' },
+        { value: 'name', label: 'Name' },
+        { value: '-released', label: 'Release' },
+        { value: '-metacritic', label: 'Popularity' },
+        { value: '-rating', label: 'Average rating' },
+    ]
+
+    const currentSortOrder = sortOrders.find(order => order.value === sortOrder);
     return (
         <Menu>
-            <MenuButton as={Button} rightIcon={<BsChevronDown />}>Order by: Relevance</MenuButton>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}>Order by: {currentSortOrder?.label || 'Relevance'}</MenuButton>
             <MenuList>
-                <MenuItem>Relevance</MenuItem>
-                <MenuItem>Data added</MenuItem>
-                <MenuItem>Name</MenuItem>
-                <MenuItem>Release</MenuItem>
-                <MenuItem>Popularity</MenuItem>
-                <MenuItem>Average rating</MenuItem>
+                {sortOrders.map(sort =>
+                    <MenuItem
+                        key={sort.value}
+                        value={sort.value}
+                        onClick={() => onSelectedSortOrder(sort.value)}>
+                        {sort.label}
+                    </MenuItem>)
+                }
             </MenuList>
         </Menu>
     )
--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,4 +1,7 @@
+import NoImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImage = (url: string) => {
+    if (!url) return NoImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
 
